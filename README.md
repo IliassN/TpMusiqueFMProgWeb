90f6da31f9d127198007711e1ae2a49c274d2056
IliassN <2277205@cegepmontpetit.ca>
2024-09-21 22:05:04 -0400

    Méthode recherche artiste
    Début du code dans le type script de app component pour la méthode rechercher un artiste et modification du html (commencement)

--- README.md
+++
@@ -1,1 +0,0 @@
-# TpMusiqueFMProgWeb
--- Tp1MusiqueFM/src/app/app.component.ts
+++ Tp1MusiqueFM/src/app/app.component.ts
@@ -1,13 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Tp1MusiqueFM';
+  result = false;
+  artist: string = "";
+  apiKey: string = "9a8a3facebbccaf363bb9fd68fa37abf";
+
+  constructor(public http: HttpClient) { }
+
+  async searchArtist(): Promise<void> {
+    this.result = true;
+    const rechercheAlbum = await lastValueFrom(this.http.get("http://ws.audioscrobbler.com/2.0/?method=artist.getTopAlbums&artist=" + this.artist + "&api_key=" + this.apiKey + "&format=json"));
+
+  }
+
 }
