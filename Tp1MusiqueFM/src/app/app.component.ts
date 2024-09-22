10d4ff6e8a063040f913db08767a4e63b840934c
IliassN <2277205@cegepmontpetit.ca>
2024-09-22 01:21:38 -0400

    Classe album et récupération des Top Albums
    Création de la classe album et complétion du code dans le typeScript de app component pour récupérer les top albums dans la console et les rajouter à la liste des albums

--- Tp1MusiqueFM/src/app/app.component.ts
+++ Tp1MusiqueFM/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -15,14 +16,26 @@ export class AppComponent {
   title = 'Tp1MusiqueFM';
   result = false;
   artist: string = "";
+  albums: { name: string, image: string }[] = [];
+  musiques: string[] = [];
   apiKey: string = "9a8a3facebbccaf363bb9fd68fa37abf";
 
   constructor(public http: HttpClient) { }
 
   async searchArtist(): Promise<void> {
     this.result = true;
-    const rechercheAlbum = await lastValueFrom(this.http.get("http://ws.audioscrobbler.com/2.0/?method=artist.getTopAlbums&artist=" + this.artist + "&api_key=" + this.apiKey + "&format=json"));
+    let rechercheAlbum = await lastValueFrom(this.http.get("http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" + this.artist + "&api_key=" + this.apiKey + "&format=json"));
+    this.albums = [];
+    console.log(rechercheAlbum)
 
+
+    for (let i = 0; i < rechercheAlbum.topalbums.album.length; i++) {
+      const album = rechercheAlbum.topalbums.album[i];
+      this.albums.push({
+        name: album.name,
+        image: album.image[3]['#text']
+      });
+    }
   }
 
 }
---
+++ Tp1MusiqueFM/src/models/album.ts
@@ -0,0 +1,11 @@
+export class Album {
+    name: string;
+    artist: string;
+    image: string;
+
+    constructor(name: string, artist: string, image: string) {
+        this.name = name;
+        this.artist = artist;
+        this.image = image;
+    }
+}
